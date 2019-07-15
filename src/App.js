443067a02498c9abe01f025b103bcc55f6b2dcb6
Alynx Zhou <alynx.zhou@gmail.com>
2019-07-15 15:25:46 +0800

    Replace tab with 2 spaces.

--- src/App.js
+++ src/App.js
@@ -7,49 +7,49 @@ import Session from './Session'
 import Container from '@material-ui/core/Container'
 
 class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			'status': Status.LAUNCH,
-			'socket': null,
-			'server': null,
-			'wid': null,
-			'sid': null
-		}
-		this.passProps = {
-			'getAppState': this.getAppState.bind(this),
-			'setAppState': this.setAppState.bind(this)
-		}
-	}
-	getAppState() {
-		return this.state
-	}
-	setAppState(obj) {
-		return this.setState(obj)
-	}
-	renderContent() {
-		switch (this.state.status) {
-			case Status.LIST:
-				return <List {...this.passProps} />
-			case Status.SESSION_CREATE:
-				return <Session {...this.passProps} />
-			case Status.SESSION_JOIN:
-				return <Session isWatching={false} {...this.passProps} />
-			case Status.SESSION_WATCH:
-				return <Session isWatching={true} {...this.passProps} />
-			default:
-				return <Launch {...this.passProps} />
-		}
-	}
-	render() {
-		return (
-			<Container maxWidth="md">
-				<div className="app">
-					{this.renderContent()}
-				</div>
-			</Container>
-		)
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      'status': Status.LAUNCH,
+      'socket': null,
+      'server': null,
+      'wid': null,
+      'sid': null
+    }
+    this.passProps = {
+      'getAppState': this.getAppState.bind(this),
+      'setAppState': this.setAppState.bind(this)
+    }
+  }
+  getAppState() {
+    return this.state
+  }
+  setAppState(obj) {
+    return this.setState(obj)
+  }
+  renderContent() {
+    switch (this.state.status) {
+      case Status.LIST:
+        return <List {...this.passProps} />
+      case Status.SESSION_CREATE:
+        return <Session {...this.passProps} />
+      case Status.SESSION_JOIN:
+        return <Session isWatching={false} {...this.passProps} />
+      case Status.SESSION_WATCH:
+        return <Session isWatching={true} {...this.passProps} />
+      default:
+        return <Launch {...this.passProps} />
+    }
+  }
+  render() {
+    return (
+      <Container maxWidth="md">
+        <div className="app">
+          {this.renderContent()}
+        </div>
+      </Container>
+    )
+  }
 }
 
 export default App
--- src/Launch.js
+++ src/Launch.js
@@ -4,23 +4,23 @@ import Status from './Status'
 import AddressForm from './AddressForm'
 
 class Launch extends React.Component {
-	onSubmit(server) {
-		const socket = io(server)
-		this.props.setAppState({server, socket})
-		socket.on('watcher', (msg) => {
-			const data = JSON.parse(msg)
-			const {wid} = data
-			this.props.setAppState({'status': Status.LIST, wid})
-		})
-		socket.emit('watcher', JSON.stringify({}))
-	}
-	render() {
-		return (
-			<AddressForm
-			  onSubmit={this.onSubmit.bind(this)}
-				value={this.props.getAppState().server || 'http://localhost:3003'} />
-		)
-	}
+  onSubmit(server) {
+    const socket = io(server)
+    this.props.setAppState({server, socket})
+    socket.on('watcher', (msg) => {
+      const data = JSON.parse(msg)
+      const {wid} = data
+      this.props.setAppState({'status': Status.LIST, wid})
+    })
+    socket.emit('watcher', JSON.stringify({}))
+  }
+  render() {
+    return (
+      <AddressForm
+        onSubmit={this.onSubmit.bind(this)}
+        value={this.props.getAppState().server || 'http://localhost:3003'} />
+    )
+  }
 }
 
 export default Launch
